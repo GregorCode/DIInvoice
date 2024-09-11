b3df6e4debcb01c01e7fd1e69273ad46e64f91cf
Gregorys Laptop <gregorysgonzalez@gmail.com>
2024-09-11 18:36:52 -0300

    mando nuevo metodo example

--- src/main/java/com/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -8,6 +8,7 @@ import com.curso.springboot.di.factura.springboot_difactura.models.Invoice;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @RestController
 @RequestMapping("/invoices")
 public class InvoiceController {
@@ -19,4 +20,10 @@ public class InvoiceController {
     public Invoice show() {
         return invoice;
     }
+
+    @GetMapping("/example")
+    public String example () {
+        return "hola como estas";
+    }
+    
 }
