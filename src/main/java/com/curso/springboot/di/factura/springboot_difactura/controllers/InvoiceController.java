b5dc2426728c9244d3a0cfbc252e8bf56e70758c
Gregorys Laptop <gregorysgonzalez@gmail.com>
2024-09-11 21:34:50 -0300

    finalizo el proyecto

--- src/main/java/com/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
+++ src/main/java/com/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -4,10 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.springboot.di.factura.springboot_difactura.models.Client;
 import com.curso.springboot.di.factura.springboot_difactura.models.Invoice;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
 
 @RestController
 @RequestMapping("/invoices")
@@ -18,7 +17,17 @@ public class InvoiceController {
 
     @GetMapping("/show")
     public Invoice show() {
-        return invoice;
+        
+        Client client = new Client();
+        client.setName(invoice.getClient().getName());
+        client.setLastName(invoice.getClient().getLastName());
+
+        Invoice inv = new Invoice();
+        inv.setClient(client);
+        inv.setDescription(invoice.getDescription());
+        inv.setItems(invoice.getItems());
+
+        return inv;
     }
 
     @GetMapping("/example")
